b47a40f5448ba87fb464d619d25e126f84d18fc9
Saurav Raj <saurved785@gmail.com>
2022-08-26 13:33:16 +0530

    minor fix

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ $(window).scroll(() => {
 	if (scroll !== 0) {
 		console.log("active");
 
-		$("nav").css({ "background-color": "#ffffff", transition: "0.2s" });
+		$("nav").css({ "background-color": "#ffffff", transition: "0.3s" });
 	} else {
-		$("nav").css({ "background-color": "rgba(0,0,0,0)", transition: "0.2s" });
+		$("nav").css({ "background-color": "rgba(0,0,0,0)", transition: "0.3s" });
 	}
 });
 console.log(hi);
